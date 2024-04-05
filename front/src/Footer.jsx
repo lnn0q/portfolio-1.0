ee1ccef4151779f525db4084dffe7e8c65ff16ac
lnn0q <denysenko.e0@gmail.com>
2024-04-05 23:31:23 +0300

    Added header and footer

--- front/src/Footer.jsx
+++ front/src/Footer.jsx
@@ -1,5 +1,13 @@
 const Footer = () => {
-  return <footer>Footer</footer>;
+  let year = new Date().getFullYear();
+  return (
+    <footer>
+      <hr />
+      <div className="footer__label">
+        &copy; {2024 !== year ? "2024 - " + year : "2024"}
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- front/src/Header.jsx
+++ front/src/Header.jsx
@@ -1,5 +1,19 @@
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div className="header__previewBox">
+        <img src="/assets/images/background.jpg" className="header__img" />
+        <div className="header__labelModal">
+          <div className="header__labelTyped">Welcome!</div>
+        </div>
+      </div>
+      <nav className="header__nav">
+        <a href="#1">About</a>
+        <a href="#2">Contacts</a>
+        <a href="#3">Projects</a>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
